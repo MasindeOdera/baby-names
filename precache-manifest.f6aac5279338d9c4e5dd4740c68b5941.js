8d4c50c1eff718961864839b80a2d8bd82cfe27f
MasindeOdera <MasindeOdera@gmail.com>
2020-08-26 17:25:34 +0200

    Updates

---
+++ precache-manifest.f6aac5279338d9c4e5dd4740c68b5941.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f0cd372e9b7a39de6851103caf9190",
+    "url": "/baby-names/index.html"
+  },
+  {
+    "revision": "9c8f73f304759e436e6c",
+    "url": "/baby-names/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c4a0efd51fa10c05ada",
+    "url": "/baby-names/static/js/2.80610676.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/baby-names/static/js/2.80610676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c8f73f304759e436e6c",
+    "url": "/baby-names/static/js/main.1de5b371.chunk.js"
+  },
+  {
+    "revision": "76e0ba2f8e2a2f6d8cfc",
+    "url": "/baby-names/static/js/runtime-main.5afbe58c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/baby-names/static/media/logo.5d5d9eef.svg"
+  }
+]);
